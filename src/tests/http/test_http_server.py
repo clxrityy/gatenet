7ead1049584699f7dbd3bf6540e04a191b35f4df
clxrityy <clxrityxyz@gmail.com>
2025-05-22 16:19:32 -0400

    move func to utils

--- src/tests/http/test_http_server.py
+++ src/tests/http/test_http_server.py
@@ -1,12 +1,7 @@
 import time
 import requests
 from gatenet.http.base import HTTPServerComponent
-import socket
-
-def get_free_port():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("127.0.0.1", 0))
-        return s.getsockname()[1]
+from gatenet.utils.net import get_free_port
 
     
 def test_custom_route_json():
