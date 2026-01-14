463714553114ce76a9ea11dd172c2f785a549670
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-01-13 21:39:10 -0500

    (refactor) update ARP discovery test to use ArpDiscovery class

--- tests/discovery/test_arp.py
+++ tests/discovery/test_arp.py
@@ -1,8 +1,8 @@
-from gatenet.discovery.arp import discover_arp
+from gatenet.discovery.arp import ArpDiscovery
 from typing import List
 
 
 def test_parse_arp_output_basic():
-    devices = discover_arp()
+    devices = ArpDiscovery().discover()
     assert isinstance(devices, List)
     assert all(hasattr(device, "ip") for device in devices)
